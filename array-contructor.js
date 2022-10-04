c8d82a1495fdb4f5c7faf50cf71f6d65514d73dc
Hồ Việt Đức <112045195+shinducql@users.noreply.github.com>
2022-10-04 22:44:36 +0700

    comit
    comit

---
+++ array-contructor.js
@@ -0,0 +1,4 @@
+var colors = new Array(items: 'white', 'black', 'custom');
+
+var el =document.getElementById( elementld: 'colors');
+el.textContent = colors[0];
---
+++ string-variable.js
@@ -0,0 +1,13 @@
+var  username;
+var message;
+
+username = 'Molly';
+message = 'See our upcoming range';
+
+var elName = document.getElementById( elementld: 'name');
+
+elName.textContent = username;
+
+var elNote = document.getElementById( elementld: 'note');
+
+elNote.textContent = message;
